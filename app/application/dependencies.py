8e562631efe1b9dee72361e4c9347117aae76ec5
Pavel Kozhemjachenko <pkozhem@gmail.com>
2025-03-05 20:38:11 +0300

    Expand configs. DB and session utils. Docker compose file.

---
+++ app/application/dependencies.py
@@ -0,0 +1,10 @@
+from typing import Annotated, Type
+
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.config import Settings, get_settings
+from app.infrastructure.database.session import get_async_session
+
+AsyncSessionType: Type[AsyncSession] = Annotated[AsyncSession, Depends(get_async_session)]
+SettingsType: Type[Settings] = Annotated[Settings, Depends(get_settings)]
--- app/config.py
+++ app/config.py
@@ -1,14 +1,83 @@
 from functools import lru_cache
+from pathlib import Path
 
+from pydantic import RedisDsn
+from pydantic.types import DirectoryPath
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from sqlalchemy import AsyncAdaptedQueuePool, URL
+
+PROJECT_PATH: DirectoryPath = Path(__file__).parent.parent
 
 
-class _Settings(BaseSettings):
-    email: str = "sample@email.com"
+class _ApplicationSettings(BaseSettings):
+    CLIENT_ID: str = ""
+    CLIENT_SECRET: str = ""
 
-    model_config = SettingsConfigDict(env_file="../.env")
+
+class _DatabaseSettings(BaseSettings):
+    DB_HOST: str = ""
+    DB_PORT: int = ""
+    DB_NAME: str = ""
+    DB_USERNAME: str = ""
+    DB_PASSWORD: str = ""
+    DB_DRIVER_NAME: str = ""
+    DB_POOL_SIZE: int = 10
+
+    @property
+    def uri(self) -> URL:
+        """URI for database connection."""
+
+        return URL.create(
+            drivername=self.DB_DRIVER_NAME,
+            username=self.DB_USERNAME,
+            password=self.DB_PASSWORD,
+            host=self.DB_HOST,
+            port=self.DB_PORT,
+            database=self.DB_NAME,
+        )
+
+    @property
+    def engine_kwargs(self):
+        return {
+            "pool_size": self.DB_POOL_SIZE,
+            "pool_use_lifo": True,
+            "pool_pre_ping": True,
+            "connect_args": {
+                "prepare_threshold": None,
+            },
+            "poolclass": AsyncAdaptedQueuePool,
+        }
+
+
+class _RedisSettings(BaseSettings):
+    DSN: RedisDsn | None = None
+    USE_FAKE_REDIS: bool = False
+    SOCKET_CONNECT_TIMEOUT: int = 5
+    RETRY_ON_TIMEOUT: bool = True
+    SOCKET_KEEPALIVE: bool = True
+
+
+class _TaskiqSettings(BaseSettings):
+    BROKER_URL: str = ""
+    RESULT_BACKEND: str = ""
+
+
+class Settings(BaseSettings):
+    PROJECT_PATH: DirectoryPath = PROJECT_PATH
+    APPLICATION: _ApplicationSettings = _ApplicationSettings()
+    DATABASE: _DatabaseSettings = _DatabaseSettings()
+    REDIS: _RedisSettings = _RedisSettings()
+    TASKIQ: _TaskiqSettings = _TaskiqSettings()
+
+    model_config = SettingsConfigDict(
+        env_file="../.env",
+        case_sensitive=True,
+        env_prefix="",
+        env_nested_delimiter="__",
+        extra="allow",
+    )
 
 
 @lru_cache
 def get_settings() -> BaseSettings:
-    return _Settings()
+    return Settings()
